46514e7cf6cd042d0b4ba47860a4205888558532
heyFelipao <hey.felipao@gmail.com>
2021-07-14 10:26:27 -0300

    allSettle to see how it works

---
+++ index.js
@@ -0,0 +1,26 @@
+const delay = n => new Promise(resolve => setTimeout(resolve, n));
+
+const promises = [
+    delay(2000).then(() => 1),
+    delay(1000).then(() => 2),
+    delay(3000).then(() => {
+        throw new Error('Boom');
+    }),
+];
+
+// Promise.all(promises).then(console.log).catch(console.error);
+// Error: Boom
+//     at /home/heyfelipao/Code/javascript/using-promise-allSettled/index.js:7:15
+//     at async Promise.all (index 2)
+
+// Promise.race(promises).then(console.log).catch(console.error);
+// retorna a promise que mais rapido for resolvida
+
+// Promise.allSettled(promises)
+//     .then(res => res.filter(p => p.status === 'fulfilled'))
+//     .then(res => res.map(p => p.value))
+//     .then(console.log);
+
+Promise.allSettled(promises)
+    // .then(res => res.map(p => console.log(`Status: ${p.status} - value: ${p.value}`)));
+    .then(res => console.log(res));
